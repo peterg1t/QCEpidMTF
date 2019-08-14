10b62f2882b9e9541958f2a9849c27e17802ac24
peterg1t <pedro.enrique.83@gmail.com>
2019-08-13 19:46:47 -0600

    exploring different ways to obtain the ROIs

--- qc-epid.py
+++ qc-epid.py
@@ -30,7 +30,7 @@ import numpy as np
 import argparse
 import cv2
 from skimage.feature import blob_log
-
+from math import *
 
 
 
@@ -74,6 +74,28 @@ def viewer(volume, dx, dy,center):
 
 
 
+def shape_detect(c):
+    shape='unidentified'
+    peri=cv2.arcLength(c,True)
+    approx=cv2.approxPolyDP(c) #number of vertices in the contour
+    if len(approx)==3:
+        shape='triangle'
+    elif len(approx)==4:
+        #compute the bounding box of the contour and find the aspect ratio
+        (x,y,w,h)=cv2.boundingRect(approx)
+        ar=w/float(h)
+
+        shape='square' if ar >=0.95 and ar <=1.05 else 'rectangle'
+    else:
+        shape='circle'
+
+    return shape
+
+
+
+
+
+
 
 def read_dicom(filename,ioption):
     if os.path.splitext(filename)[1] == '.dcm':
@@ -105,9 +127,79 @@ def read_dicom(filename,ioption):
 
         ArrayDicom=ArrayDicom.astype(np.uint8)
 
+
+
+
+#-----------------------------------------------------------------------------------------------
+        #performing bilateral filtering to remove some noise without affecting the edges
+        img_bifilt=cv2.bilateralFilter(ArrayDicom,11,17,17)
+        edged=cv2.Canny(img_bifilt,30,200)
+
+        # #thresholding the image to find the square
+        # th=cv2.threshold(img_bifilt,120,255,cv2.THRESH_TRUNC)[1]
+        # th2=cv2.adaptiveThreshold(th,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+        # th2=cv2.bitwise_not(th2)
+        # # edges=cv2.Canny(th2,60,120)
+
+        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        # img=cv2.morphologyEx(th2, cv2.MORPH_CLOSE, kernel)
+
+        contours=cv2.findContours(edged.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+        for cnt in contours:
+            print(cv2.contourArea(cnt))
+        # print('contours_detected=',len(contours))
+
+
+
+        cv2.drawContours(edged,contours, 0, (10,0,160), 3)
+        # cv2.imshow('img',th2)
+        # cv2.waitKey(0)
+        # # cv2.imshow('img',img_bifilt)
+        # # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+        # th=cv2.adaptiveThreshold(ArrayDicom,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+        plt.figure()
+        plt.imshow(ArrayDicom)
+        plt.title('original')
+
+        plt.figure()
+        plt.imshow(edged)
+        plt.title('bilinear filtered')
+        #
+        # plt.figure()
+        # plt.imshow(th)
+        # plt.title('threshold applied')
+        #
+        # plt.figure()
+        # plt.imshow(th2)
+        # plt.title('2 threshold')
+        #
+        # # plt.figure()
+        # # plt.imshow(edges)
+        # # plt.title('edges detected')
+        #
+        # # plt.figure()
+        # # plt.imshow(img)
+        # # plt.title('morphology applied (dil&ero)')
+
+        plt.show()
+
+
+
+        exit(0)
+# -----------------------------------------------------------------------------------------------
+
+
+
+
+
+        #lets fin the circles in this QC3 phantom (Only for Manitoba QC3 the Standard imaging one does not have them)
+
         circles=cv2.HoughCircles(ArrayDicom,cv2.HOUGH_GRADIENT,2,50,param1=100,param2=40,minRadius=2,maxRadius=15)
         circles=np.uint16(np.around(circles))
-        print(circles,np.shape(circles))
 
 
         centerXRegion = []
@@ -125,13 +217,40 @@ def read_dicom(filename,ioption):
 
     #Now that we have correctly detected the points we need to estimate the scaling of the image and the location of every ROI
 
+    x1,y1=center[0]
+    x2,y2=center[1]
+
+    theta=atan(abs(y2-y1)/abs(x2-x1))
+    theta_deg=degrees(theta)
+
+    print('theta_deg (angle the phantom was placed) =',theta_deg)
+
+    #The ROIs location can be identified by its positions with respect to the two points
 
 
+    # #Let's create a mask
+    # mask=np.zeros(np.shape(ArrayDicom),dtype=np.uint8)
+    # roi1_corners=np.array([[ (525,330),(571,375),(506,442) ,(458,395) ]],dtype=np.int32)
+    #
+    # cv2.fillPoly(mask,roi1_corners,255)
+    # result=cv2.bitwise_and(ArrayDicom,mask)
+    #
+    # plt.figure()
+    # plt.imshow(result)
+    # plt.title('mask')
 
-
-
-
-
+    # mser = cv2.MSER_create()
+    # regions = mser.detectRegions(ArrayDicom,None)[0]
+    # print('number of regions=',np.shape(regions))
+    # # hulls=[cv2.convexHull(p.reshape(-1,1,2)) for p in regions]
+    # hulls=cv2.convexHull(regions)
+    #
+    # cv2.polylines(ArrayDicom,hulls,1,(10,255,0))
+    #
+    # plt.figure()
+    # plt.imshow(ArrayDicom)
+    # plt.title('original')
+    # plt.show()
 
 
 
