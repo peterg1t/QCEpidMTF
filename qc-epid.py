141b1f6691a8ea12009bda93c8a0b9c930345b0f
peterg1t <pedro.enrique.83@gmail.com>
2019-08-14 17:24:52 -0600

    change MTF calculation to using the variance and use the two figures to got the random noise

--- qc-epid.py
+++ qc-epid.py
@@ -60,7 +60,7 @@ def viewer(volume, dx, dy,center):
     # img=ax.imshow(volume, extent=extent)
     img=ax.imshow(volume)
     # ax.set_xlabel('x distance [mm]')
-    # ax.set_ylabel('y distance [mm]')
+    # ax.set_ylabel('y disfilename1=args.epidtance [mm]')
     ax.set_xlabel('x pixel')
     ax.set_ylabel('y pixel')
     # ax.set_title("Phantom image=")
@@ -96,16 +96,66 @@ def shape_detect(c):
 
 
 
+def mtf_calc(ROI):
+    #nothing here yet
+    Imax=[]
+    Imin=[]
+    for region in ROI:
+        print('region_size',np.shape(region))
+        Imax.append(np.amax(region))
+        Imin.append(np.amin(region))
 
-def read_dicom(filename,ioption):
-    if os.path.splitext(filename)[1] == '.dcm':
-        dataset = pydicom.dcmread(filename)
+    print('Imax=',Imax,'Imin=',Imin,'sum=',np.asarray(Imax,dtype='int')+np.asarray(Imin,dtype='int'))
+    sub=np.asarray(Imax,dtype='int')-np.asarray(Imin,dtype='int')
+    sum=np.asarray(Imax,dtype='int')+np.asarray(Imin,dtype='int')
+
+    MTF=[]
+    LinePairs=[0.76,0.43,0.23,0.20,0.1]
+    C5=sub[len(Imax)-1]/sum[len(Imax)-1]
+    print('len(Imax)=',len(Imax))
+    for i in range(0,len(Imax)):
+        Ci=sub[i]/sum[i]
+        MTF.append(  Ci/C5  )
+        print('Ci',Ci,'C5',C5)
+
+    print('MTF=',MTF)
+
+    plt.figure()
+    plt.plot(LinePairs,MTF)
+    plt.title('rMTF plot')
+    plt.xlabel('Line pairs lp/mm')
+    plt.ylabel('MTF')
+    plt.ylim((0,1))
+    plt.xlim((0.1,0.76))
+    plt.show()
+
+
+
+
+
+
+def read_dicom(filename1,filename2,ioption):
+    if os.path.splitext(filename1)[1] == '.dcm':
+        dataset = pydicom.dcmread(filename1)
         SID = dataset.RTImageSID
         dx = 1 / (SID * (1 / dataset.ImagePlanePixelSpacing[0]) / 1000)
         dy = 1 / (SID * (1 / dataset.ImagePlanePixelSpacing[1]) / 1000)
         print("pixel spacing row [mm]=", dx)
         print("pixel spacing col [mm]=", dy)
-        ArrayDicom = dataset.pixel_array
+
+        if filename2 != '': # we average the two datasets
+            dataset2=pydicom.dcmread(filename2)
+            ArrayDicom_o = dataset.pixel_array
+            ArrayDicom = dataset.pixel_array
+            ArrayDicom2 = dataset2.pixel_array
+            ArrayDicom=(ArrayDicom+ArrayDicom2)/2
+
+        else:
+            ArrayDicom_o = dataset.pixel_array
+            ArrayDicom = dataset.pixel_array
+
+
+
 
         if ioption.startswith(('y', 'yeah', 'yes')):
             # max_val = np.amax(ArrayDicom)
@@ -133,67 +183,67 @@ def read_dicom(filename,ioption):
 
 
 
-#-----------------------------------------------------------------------------------------------
-        #performing bilateral filtering to remove some noise without affecting the edges
-        img_bifilt=cv2.bilateralFilter(ArrayDicom,11,17,17)
-        edged=cv2.Canny(img_bifilt,30,200)
-
-        # #thresholding the image to find the square
-        # th=cv2.threshold(img_bifilt,120,255,cv2.THRESH_TRUNC)[1]
-        # th2=cv2.adaptiveThreshold(th,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-        # th2=cv2.bitwise_not(th2)
-        # # edges=cv2.Canny(th2,60,120)
-
-        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
-        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        # img=cv2.morphologyEx(th2, cv2.MORPH_CLOSE, kernel)
-
-        contours=cv2.findContours(edged.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
-        for cnt in contours:
-            print(cv2.contourArea(cnt))
-        # print('contours_detected=',len(contours))
-
-
-
-        cv2.drawContours(edged,contours, 1, (10,0,160), 3)
-        # cv2.imshow('img',th2)
-        # cv2.waitKey(0)
-        # # cv2.imshow('img',img_bifilt)
-        # # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-
-        # th=cv2.adaptiveThreshold(ArrayDicom,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-        plt.figure()
-        plt.imshow(ArrayDicom)
-        plt.title('original')
-
-        plt.figure()
-        plt.imshow(edged)
-        plt.title('bilinear filtered')
-        #
-        # plt.figure()
-        # plt.imshow(th)
-        # plt.title('threshold applied')
-        #
-        # plt.figure()
-        # plt.imshow(th2)
-        # plt.title('2 threshold')
-        #
-        # # plt.figure()
-        # # plt.imshow(edges)
-        # # plt.title('edges detected')
-        #
-        # # plt.figure()
-        # # plt.imshow(img)
-        # # plt.title('morphology applied (dil&ero)')
-
-        plt.show()
-
-
-
-        exit(0)
-# -----------------------------------------------------------------------------------------------
+# #-----------------------------------------------------------------------------------------------
+#         #performing bilateral filtering to remove some noise without affecting the edges
+#         img_bifilt=cv2.bilateralFilter(ArrayDicom,11,17,17)
+#         edged=cv2.Canny(img_bifilt,30,200)
+#
+#         # #thresholding the image to find the square
+#         # th=cv2.threshold(img_bifilt,120,255,cv2.THRESH_TRUNC)[1]
+#         # th2=cv2.adaptiveThreshold(th,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+#         # th2=cv2.bitwise_not(th2)
+#         # # edges=cv2.Canny(th2,60,120)
+#
+#         # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+#         # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+#         # img=cv2.morphologyEx(th2, cv2.MORPH_CLOSE, kernel)
+#
+#         contours=cv2.findContours(edged.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+#         for cnt in contours:
+#             print(cv2.contourArea(cnt))
+#         # print('contours_detected=',len(contours))
+#
+#
+#
+#         cv2.drawContours(edged,contours, 1, (10,0,160), 3)
+#         # cv2.imshow('img',th2)
+#         # cv2.waitKey(0)
+#         # # cv2.imshow('img',img_bifilt)
+#         # # cv2.waitKey(0)
+#         # cv2.destroyAllWindows()
+#
+#
+#         # th=cv2.adaptiveThreshold(ArrayDicom,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+#         plt.figure()
+#         plt.imshow(ArrayDicom)
+#         plt.title('original')
+#
+#         plt.figure()
+#         plt.imshow(edged)
+#         plt.title('bilinear filtered')
+#         #
+#         # plt.figure()
+#         # plt.imshow(th)
+#         # plt.title('threshold applied')
+#         #
+#         # plt.figure()
+#         # plt.imshow(th2)
+#         # plt.title('2 threshold')
+#         #
+#         # # plt.figure()
+#         # # plt.imshow(edges)
+#         # # plt.title('edges detected')
+#         #
+#         # # plt.figure()
+#         # # plt.imshow(img)
+#         # # plt.title('morphology applied (dil&ero)')
+#
+#         plt.show()
+#
+#
+#
+#         exit(0)
+# # -----------------------------------------------------------------------------------------------
 
 
 
@@ -228,7 +278,69 @@ def read_dicom(filename,ioption):
 
     print('theta_deg (angle the phantom was placed) =',theta_deg)
 
+    #The distance between the centers of the ROIs in pixels is given by
+    dist_roi=int(21/dx)       #where 21 mm is the witdh of the ROI each ROI is 20 mm width with 1mm spacer and 28mm in height
+    width_roi=int(20/dx)-10 # just subtracting a few pixels to avoid edge effects
+    height_roi=int(28/dy)-10
+    print('dist_roi=',dist_roi)
+    # exit(0)
+
     #The ROIs location can be identified by its positions with respect to the two points
+
+    #let's rotate the image around the center of the first ROI
+    xrot=int(abs(x2+x1)/2)
+    yrot=int(abs(y2+y1)/2)
+
+
+    M = cv2.getRotationMatrix2D((xrot,yrot),theta_deg,1)
+    ArrayDicom_rot=cv2.warpAffine(ArrayDicom_o,M,(np.shape(ArrayDicom_o)[1],np.shape(ArrayDicom_o)[0]))
+
+    plt.figure()
+    plt.imshow(ArrayDicom_rot)
+    plt.title('rotated')
+    plt.show(block=False)
+
+    ROI=[]
+    ROI.append(ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot-int(width_roi/2):xrot+int(width_roi/2)])
+    ROI.append(ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot-dist_roi-int(width_roi/2):xrot-dist_roi+int(width_roi/2)])
+    ROI.append(ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot+dist_roi-int(width_roi/2):xrot+dist_roi+int(width_roi/2)])
+    ROI.append(ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot-2*dist_roi-int(width_roi/2):xrot-2*dist_roi+int(width_roi/2)])
+    ROI.append(ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot+2*dist_roi-int(width_roi/2):xrot+2*dist_roi+int(width_roi/2)])
+
+
+    ROI1=ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot-int(width_roi/2):xrot+int(width_roi/2)]
+    ROI2=ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot-dist_roi-int(width_roi/2):xrot-dist_roi+int(width_roi/2)]
+    ROI3=ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot+dist_roi-int(width_roi/2):xrot+dist_roi+int(width_roi/2)]
+    ROI4=ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot-2*dist_roi-int(width_roi/2):xrot-2*dist_roi+int(width_roi/2)]
+    ROI5=ArrayDicom_rot[yrot-int(height_roi/2):yrot+int(height_roi/2),xrot+2*dist_roi-int(width_roi/2):xrot+2*dist_roi+int(width_roi/2)]
+
+
+    plt.figure()
+    plt.imshow(ROI1)
+    plt.title('ROI1')
+
+    plt.figure()
+    plt.imshow(ROI2)
+    plt.title('ROI2')
+
+    plt.figure()
+    plt.imshow(ROI3)
+    plt.title('ROI3')
+
+    plt.figure()
+    plt.imshow(ROI4)
+    plt.title('ROI4')
+
+    plt.figure()
+    plt.imshow(ROI5)
+    plt.title('ROI5')
+
+    plt.show()
+
+    #now that we have the ROIs we can proceed to calculate the rMTF
+    mtf_calc(ROI)
+
+
 
 
     # #Let's create a mask
@@ -241,6 +353,9 @@ def read_dicom(filename,ioption):
     # plt.figure()
     # plt.imshow(result)
     # plt.title('mask')
+
+
+
 
     # mser = cv2.MSER_create()
     # regions = mser.detectRegions(ArrayDicom,None)[0]
@@ -260,7 +375,7 @@ def read_dicom(filename,ioption):
 
     # Normal mode:
     print()
-    print("Filename.........:", filename)
+    print("Filename.........:", filename1)
     print("Storage type.....:", dataset.SOPClassUID)
     print()
 
@@ -288,8 +403,19 @@ def read_dicom(filename,ioption):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('epid',type=str,help="Input the filename")
+parser.add_argument('-a', '--add', nargs='?', type=argparse.FileType('r'), help='additional file for averaging before processing')
 args=parser.parse_args()
-filename=args.epid
+
+filename1=args.epid
+filename2=''
+
+if args.add:
+    additional=args.add
+    filename2=additional.name
+
+
+
+
 
 while True:  # example of infinite loops using try and except to catch only numbers
     line = input('Are these files from a clinac [yes(y)/no(n)]> ')
@@ -305,4 +431,5 @@ while True:  # example of infinite loops using try and except to catch only numb
 
 
 
-read_dicom(filename,ioption)
+
+read_dicom(filename1,filename2,ioption)
